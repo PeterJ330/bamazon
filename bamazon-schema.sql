132724563bfcadb60845688319e4b5b64bf9e176
peterj330 <peter.a.jarrard@gmail.com>
2018-09-07 22:48:39 -0500

    sql updates

--- bamazon-schema.sql
+++ bamazon-schema.sql
@@ -4,7 +4,7 @@ USE bamazon;
 
 CREATE TABLE products
 (
-    item_id INT NOT NULL,
+    item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(50)NULL,
     department_name VARCHAR(50)NULL,
     price DECIMAL(10,2)NULL,
@@ -25,36 +25,3 @@ INSERT INTO products (product_name, department_name, price, stock_quantity)
      ("Product Ten", "Department Ten", 16.50, 110);
 
 SELECT * FROM products;
-
-
-
-
--- INSERT INTO products (product_name, department_name, price, stock_quantity) 
---     VALUES ("Swiffer", "Department One", 14.99, 85);
-
--- INSERT INTO products (product_name, department_name, price, stock_quantity) 
---     VALUES ("Product Two", "Department Two", 11.99, 70);
-
--- INSERT INTO products (product_name, department_name, price, stock_quantity) 
---     VALUES ("Product Three", "Department Three", 9.99, 100);
-
--- INSERT INTO products (product_name, department_name, price, stock_quantity) 
---     VALUES ("Product Four", "Department Four", 24.99, 58);
-
--- INSERT INTO products (product_name, department_name, price, stock_quantity) 
---     VALUES ("Product Five", "Department Five", 15.99, 35);
-
--- INSERT INTO products (product_name, department_name, price, stock_quantity) 
---     VALUES ("Product Six", "Department Six", 4.50, 77);
-
--- INSERT INTO products (product_name, department_name, price, stock_quantity) 
---     VALUES ("Product Seven", "Department Seven", 29.99, 90);
-
--- INSERT INTO products (product_name, department_name, price, stock_quantity) 
---     VALUES ("Product Eight", "Department Eight", 7.50, 80);
-
--- INSERT INTO products (product_name, department_name, price, stock_quantity) 
---     VALUES ("Product Nine", "Department Nine", 8.99, 52);
-
--- INSERT INTO products (product_name, department_name, price, stock_quantity) 
---     VALUES ("Product Ten", "Department Ten", 16.50, 110);
