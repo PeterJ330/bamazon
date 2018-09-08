16a84e6f1d4127f588a962b0a7b51d4e3ddb5a37
peterj330 <peter.a.jarrard@gmail.com>
2018-09-07 23:53:00 -0500

    connection fix

--- bamazon.js
+++ bamazon.js
@@ -1,18 +1,24 @@
-var mysql = require()("mysql");
+var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host: "localhost", 
-    port: 3306,
-    user: "root",
-    password:"Password1!",
-    database: "bamazon"
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Password1!",
+  database: "bamazon"
 });
 
 connection.connect(function(err) {
-    if(err) throw err;
-    console.log("You are connected as id " + connection.threadId + "\n");
-    productDisplay();
+  if (err) throw err;
+  console.log("You are connected as id " +connection.threadId + "\n");
+  productDisplay();  
 });
 
 function productDisplay() {
@@ -21,4 +27,22 @@ function productDisplay() {
         console.log(res);
     })
 };
+
+function productSearch() {
+    inquirer
+      .prompt({
+        name: "product",
+        type: "input",
+        message: "Please enter the ID of the product you would like to purchase ";
+      })
+      .then(function(answer) {
+        var query = "SELECT position, song, year FROM top5000 WHERE ?";
+        connection.query(query, { artist: answer.artist }, function(err, res) {
+          for (var i = 0; i < res.length; i++) {
+            console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+          }
+          runSearch();
+        });
+      });
+  }
 
