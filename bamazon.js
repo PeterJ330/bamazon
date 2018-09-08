0b229ce07e056f08674687e384a628f65ee8bcd0
peterj330 <peter.a.jarrard@gmail.com>
2018-09-07 23:05:23 -0500

    revert

--- bamazon.js
+++ bamazon.js
@@ -11,7 +11,7 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if(err) throw err;
-    console.log("You are connected as id " +connection.threadId + "\n");
+    console.log("You are connected as id " + connection.threadId + "\n");
     productDisplay();
 });
 
